aca6ecd4bd6cbfbcf8697ff81754bdf7242d92f2
MvLeronn <marcos23_correia@hotmail.com>
2025-03-18 12:03:19 -0300

    feat: Criado o layout da tela principal

---
+++ app/src/main/java/co/mvleronn/ganheinamega/MainActivity.kt
@@ -0,0 +1,23 @@
+package co.mvleronn.ganheinamega
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // buscar os objetos e ter a referencia deles
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        // OnClick no button
+        btnGenerate.setOnClickListener {
+        }
+    }
+}
